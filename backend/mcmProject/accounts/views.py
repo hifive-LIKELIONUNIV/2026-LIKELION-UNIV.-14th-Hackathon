69bb2838e8622ff5379e826bc128d6f27453af18
seoyeon615 <bsy070615@duksung.ac.kr>
2026-08-13 09:31:53 +0900

    feat: 온보딩 및 로그인 기능 구현

---
+++ backend/mcmProject/accounts/views.py
@@ -0,0 +1,20 @@
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'accounts/signup.html', {'form': form})
+
+
+class EmailLoginView(LoginView):
+    template_name = 'accounts/login.html'
---
+++ backend/mcmProject/onboarding/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def onboarding_view(request):
+    return render(request, 'onboarding/onboarding.html')
