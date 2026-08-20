88f08c97cabd84c9884972e937351b13056dba57
chawon0429 <chawon0429@gmail.com>
2026-08-21 08:21:56 +0900

    FinalPage, 모달창 수정

--- frontend/src/pages/TimeMachinePage/TimeMachinePage.jsx
+++ frontend/src/pages/TimeMachinePage/TimeMachinePage.jsx
@@ -105,7 +105,7 @@ function TimeMachinePage() {
         />
 
         <div className="tm-page__content">
-          <h1 className="tm-page__title">타임머신이 시작되었습니다.</h1>
+          <h1 className="tm-page__title">타임포탈이 시작되었습니다.</h1>
           <p className="tm-page__subtitle">곧 얼굴 인식 화면으로 이동합니다.</p>
 
           <div className="tm-postcard">
